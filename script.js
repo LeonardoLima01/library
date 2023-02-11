5ab0e0095837df103865af374820736341f5abbd
socry <leonardosbo02@gmail.com>
2023-02-11 07:47:46 -0300

    Add variables and pop-up form

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
-addBookButton = document.querySelector(".addButton>button");
-container = document.querySelector(".container");
-popupForm = document.querySelector(".popup");
-body = document.querySelector("body");
+// Variables
+let addBookButton = document.querySelector(".addButton>button");
+let container = document.querySelector(".container");
+let popupForm = document.querySelector(".popup");
+let body = document.querySelector("body");
+let form = document.querySelector("form");
+let booksGrid = document.querySelector(".booksGrid");
+let deletedCards = 0;
+let count = 0;
 
+// Show/hide pop-up form
 addBookButton.addEventListener("click", () => {
   container.style.display = "none";
   popupForm.style.display = "flex";
   body.style.display = "flex";
 });
 
-let myLibrary = [];
+// Get and redirect forms input
+form.addEventListener("submit", function (event) {
+  event.preventDefault(); // Prevents the form from autosubmitting
 
-function Book() {
-  // the constructor...
-}
+  let title = document.querySelector("#title").value;
+  let author = document.querySelector("#author").value;
+  let pages = document.querySelector("#pages").value;
+  let read = document.querySelector("#read");
 
-function addBookToLibrary() {
-  // do stuff here
-}
-
-function displayBooks() {
-  // display books iterating over myLibrary array
-}
+  let book = new Book(title, author, pages, read.checked);
+  addBookToLibrary(book);
+});
